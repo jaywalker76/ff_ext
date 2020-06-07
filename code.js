b4844fee7fa5e1ad136906d56dd4610f082a0c6d
JSCC <juliocosta1@gmail.com>
2020-06-07 20:10:54 +0100

    first commit

--- code.js
+++ code.js
@@ -1,1 +1,57 @@
-document.body.style.border = "5px solid red";
+const getPageInfoCallback = () => {};
+
+// async function logListener(info) {
+//   try {
+//     // let tabInfo = await browser.tabs.get(info.tabId);
+//     .sendMessage(tab.id, { target: "packd", data: { action: "pack" } })
+//     console.log(tabInfo);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// browser.tabs.onActivated.addListener(logListener);
+
+// async successCallback(tabs){
+//   let windowRecord = [];
+
+//   tabs.forEach((tab) => {
+//     //
+
+//     browser.tabs
+//       .sendMessage(tab.id, { target: "packd", data: { action: "pack" } })
+//       .then((pack) => {
+//         console.log("pack: " + pack);
+//         // windowRecord.push(
+//         //   Object.assign(
+//         //     {
+//         //       pack: pack,
+//         //     }
+//         //     // tabInfoToRecord(await browser.tabs.get(getTabId(tabEntry)))
+//         //   )
+//         // );
+//       })
+//       .catch((reason) => {
+//         // windowRecord.push(
+//         //   Object.assign(
+//         //     {
+//         //       pack: undefined,
+//         //     }
+//         //     // tabInfoToRecord(await browser.tabs.get(getTabId(tabEntry)))
+//         //   )
+//         // );
+//       });
+//   });
+// };
+const failureCallback = () => console.log("Something went wrong");
+
+async function successCallback(tabs) {
+  for (const tab of tabs) {
+    const tabInfo = await browser.tabs.get(tab.id);
+    console.log(tabInfo);
+  }
+}
+
+let allTabs = browser.tabs
+  .query({ currentWindow: true })
+  .then(successCallback, failureCallback);
